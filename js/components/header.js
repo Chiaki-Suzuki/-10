d5e05e623ae0504fc88cddccd86259053e25b34e
Chiaki-Suzuki <c.suzuki.eba@gmail.com>
2022-09-28 16:44:46 +0900

    spナビをjqueryなしで表示非表示を切り替えるように修正

--- js/components/header.js
+++ js/components/header.js
@@ -3,7 +3,7 @@ let itemHeader = {
   <header class="header">
   <div class="header_inner">
       <h1><a href="#html"><img src="images/logo_blue.png" alt="リーガルコンタクト"></a></h1>
-      <div id="hamburger" class="hamburger sp_only"><img src="images/hamburger_menu.png" alt="メニューアイコン"></div>
+      <div id="hamburger" class="hamburger sp_only" @click="navShow"><img src="images/hamburger_menu.png" alt="メニューアイコン"></div>
       <div class="flexbtn pc_only">
           <div class="flexbtn_1">
               <a href="mailto:koyamajimusyo@legal-contact.com">
@@ -38,24 +38,41 @@ let itemHeader = {
       </div>
   </div>
 
-  <div class="sp_only section_9">
-    <div class="nav_header">
-        <h1><a href="#top"><img src="images/logo_white.png" alt="リーガルコンタクト"></a></h1>
-        <div id="cross" class="cross"><img src="images/cross.png" alt="クロスアイコン"></div>
+  <transition name="spnav">
+    <div class="sp_only section_9" v-if="isShow">
+      <div class="nav_header">
+          <h1><a href="#top"><img src="images/logo_white.png" alt="リーガルコンタクト"></a></h1>
+          <div id="cross" class="cross"><img src="images/cross.png" alt="クロスアイコン" @click="navClose"></div>
+      </div>
+      <div class="inner">
+          <ul class="nav_sp">
+              <li><a href="#section_1" @click="navClose">当事務所について</a></li>
+              <li><a href="#section_2" @click="navClose">経営理念</a></li>
+              <li><a href="#section_3" @click="navClose">代表挨拶</a></li>
+              <li><a href="#section_4" @click="navClose">業務内容</a></li>
+              <li><a href="#section_5" @click="navClose">ご依頼の流れ</a></li>
+              <li><a href="#section_7" @click="navClose">事務所概要</a></li>
+              <li><a href="#section_6" @click="navClose">アクセス</a></li>
+          </ul>
+      </div>
+      <p><small>Copyright 2016&nbsp;LEGAL&nbsp;CONTACT&nbsp;司法書士小山合同事務所</small></p>
     </div>
-    <div class="inner">
-        <ul class="nav_sp">
-            <li><a href="#section_1">当事務所について</a></li>
-            <li><a href="#section_2">経営理念</a></li>
-            <li><a href="#section_3">代表挨拶</a></li>
-            <li><a href="#section_4">業務内容</a></li>
-            <li><a href="#section_5">ご依頼の流れ</a></li>
-            <li><a href="#section_7">事務所概要</a></li>
-            <li><a href="#section_6">アクセス</a></li>
-        </ul>
-    </div>
-    <p><small>Copyright 2016&nbsp;LEGAL&nbsp;CONTACT&nbsp;司法書士小山合同事務所</small></p>
-  </div>
+  </transition>
   </header>
-  `
+  `,
+  data: () => {
+    return {
+      isShow: false
+    }
+  },
+  methods: {
+    // spナビ表示
+    navShow: function () {
+      this.isShow = true
+    },
+    // spナビ非表示
+    navClose: function () {
+      this.isShow = false
+    }
+  }
 };
